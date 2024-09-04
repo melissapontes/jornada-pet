b3a3ae65f7c75bd8760d6bb47413d9065e29cee0
melissapontes <Melissa.pontes@gmail.com>
2024-09-04 11:46:39 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,33 @@
+//Cada paciente é um Objeto
+
+let pacientes = [
+    {
+        nome: "Oscar",
+        especie: "Felina",
+        idade: "10 anos",
+        estadiamento: "DRC 1",
+        tutor: "Angélica Soares"
+    },
+    {
+        nome: "Linda",
+        especie: "Canina",
+        idade: "6 anos",
+        estadiamento: "DRC 2",
+        tutor: "Paulo Cesar"
+    },
+    {
+        nome: "Nina",
+        especie: "Felina",
+        idade: "3 anos",
+        estadiamento: "DRC 1",
+        tutor: "Maria Cláudia"
+    },
+    {
+        nome: "Petisco",
+        especie: "Canina",
+        idade: "3 anos",
+        estadiamento: "DRC 1",
+        tutor: "Aline Araújo"
+    },
+];
+
